abd78aee0e005dc57ea7ed0007bcd908c0fb319d
larswww <lars.woldern@gmail.com>
2022-10-30 14:28:21 +0100

    simple example for basic vs code get started debug

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # hyf-vscode-debug-example
+
---
+++ debugme.js
@@ -0,0 +1,12 @@
+
+export const consoleLogger = () => {
+    console.log('hack your future!')
+    const test = 1
+
+    debugger
+
+    console.log('today')
+    
+}
+
+consoleLogger()
